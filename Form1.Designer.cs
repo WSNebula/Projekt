4720ffaee0110d89a32b5787b1484351935e869e
WSNebula <wsnebula@gmail.com>
2024-05-14 08:29:35 +0200

    added the basic elements for the skatteberegner this includes textboxes, labels and buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,14 @@
             button1 = new Button();
             label2 = new Label();
             label5 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            textBoxMåned2 = new TextBox();
+            label6 = new Label();
+            SubmitMåned2 = new Button();
+            textBoxOutput2 = new TextBox();
+            label7 = new Label();
+            checkBox1 = new CheckBox();
             SuspendLayout();
             // 
             // Submit
@@ -73,6 +81,7 @@
             textBoxOutput.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
             textBoxOutput.Location = new Point(80, 251);
             textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.ReadOnly = true;
             textBoxOutput.Size = new Size(100, 21);
             textBoxOutput.TabIndex = 14;
             // 
@@ -122,16 +131,106 @@
             label5.ForeColor = SystemColors.Control;
             label5.Location = new Point(365, 45);
             label5.Name = "label5";
-            label5.Size = new Size(102, 14);
+            label5.Size = new Size(95, 14);
             label5.TabIndex = 20;
-            label5.Text = "Antal penge i kr.";
+            label5.Text = "Månedsløn i kr.";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(221, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 14);
+            label1.TabIndex = 21;
+            label1.Text = "UdgiftBeregner";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(715, 27);
+            label3.Name = "label3";
+            label3.Size = new Size(88, 14);
+            label3.TabIndex = 22;
+            label3.Text = "SkatBeregner";
+            // 
+            // textBoxMåned2
+            // 
+            textBoxMåned2.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxMåned2.Location = new Point(632, 65);
+            textBoxMåned2.Name = "textBoxMåned2";
+            textBoxMåned2.Size = new Size(100, 21);
+            textBoxMåned2.TabIndex = 23;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = SystemColors.Control;
+            label6.Location = new Point(632, 45);
+            label6.Name = "label6";
+            label6.Size = new Size(150, 14);
+            label6.TabIndex = 24;
+            label6.Text = "Månedsløn i kr. (før skat)";
+            // 
+            // SubmitMåned2
+            // 
+            SubmitMåned2.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
+            SubmitMåned2.Location = new Point(632, 106);
+            SubmitMåned2.Name = "SubmitMåned2";
+            SubmitMåned2.Size = new Size(100, 26);
+            SubmitMåned2.TabIndex = 25;
+            SubmitMåned2.Text = "Submit";
+            SubmitMåned2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxOutput2
+            // 
+            textBoxOutput2.Location = new Point(632, 251);
+            textBoxOutput2.Name = "textBoxOutput2";
+            textBoxOutput2.ReadOnly = true;
+            textBoxOutput2.Size = new Size(100, 23);
+            textBoxOutput2.TabIndex = 26;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.Control;
+            label7.Location = new Point(738, 254);
+            label7.Name = "label7";
+            label7.Size = new Size(177, 14);
+            label7.TabIndex = 27;
+            label7.Text = "Penge at betale i skat |output|";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Arial Rounded MT Bold", 8.75F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox1.ForeColor = SystemColors.Control;
+            checkBox1.Location = new Point(755, 65);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(260, 18);
+            checkBox1.TabIndex = 28;
+            checkBox1.Text = "Er du medlem af folkekirken? (kirkeskat)";
+            checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(50, 50, 120);
+            BackColor = Color.FromArgb(64, 64, 64);
             ClientSize = new Size(1010, 639);
+            Controls.Add(checkBox1);
+            Controls.Add(label7);
+            Controls.Add(textBoxOutput2);
+            Controls.Add(SubmitMåned2);
+            Controls.Add(label6);
+            Controls.Add(textBoxMåned2);
+            Controls.Add(label3);
+            Controls.Add(label1);
             Controls.Add(label5);
             Controls.Add(label2);
             Controls.Add(button1);
@@ -158,5 +257,13 @@
         private Button button1;
         private Label label2;
         private Label label5;
+        private Label label1;
+        private Label label3;
+        private TextBox textBoxMåned2;
+        private Label label6;
+        private Button SubmitMåned2;
+        private TextBox textBoxOutput2;
+        private Label label7;
+        private CheckBox checkBox1;
     }
 }
